901d6420139f4c7e1c884fd72b7ea55087f58333
bahag-ongwl <wei-lin.ong@bahag.com>
2023-04-11 15:35:34 +0200

    update

--- pages/comments/index.tsx
+++ pages/comments/index.tsx
@@ -9,11 +9,31 @@ function CommentList() {
     fetch("/api/comments")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setComments(data);
         setLoading(false);
       });
   }, []);
+
+  const submitComment = async (comment: any) => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const deleteComment = async (commentId: any) => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    // fetchComments();
+  };
 
   if (isLoading) return <h1>Loading...</h1>;
 
@@ -34,12 +54,15 @@ function CommentList() {
 export default CommentList;
 
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/api/comments");
-  const comments = await res.json();
 
-  return {
-    props: {
-      comments,
-    },
-  };
+  /* NOT RECOMMENDED and IMPOSSIBLE */
+  /* SHOULD CALL DB DIRECTLY */
+  // const res = await fetch("http://localhost:3000/api/comments");
+  // const comments = await res.json();
+
+  // return {
+  //   props: {
+  //     comments,
+  //   },
+  // };
 }
